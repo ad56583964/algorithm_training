206e781a29d79b0666311f96f6bac072c1915bd4
ad56583964 <ad56583964@outlook.com>
2021-05-01 02:17:09 +0800

    1-11

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
-#include<stdio.h>
+//1-11
+
+#include<cstdio>  //in cpp needed
+
 int main ()
 {
-    int a, b;
+    int a, b, n, m;
+    scanf("%d%d", &n, &m);
 
-    scanf("%d%d", &a, &b);
-    printf("%d %d\n", b, a);
+    //联立方程得到
+    a = (4*n-m) /2;
+    b = n-a;
+
+    //附加实际意义的约束
+    if(m % 2 == 1 || a < 0 || b < 0)  // ||运算符具有短路策略
+        printf("No answer\n");
+    else
+        printf("%d %d\n", a, b);
 
     return 0;
 }
+
+//
