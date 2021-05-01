45b6b8a91add2c20183de4708e26e4dcac0f7a54
ad56583964 <ad56583964@outlook.com>
2021-05-01 23:05:00 +0800

    1-12(2)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//1-12
+//1-12(2)
 
 #include<cstdio>  //in cpp needed
 
@@ -8,12 +8,12 @@ int main ()
 
     scanf("%d%d%d", &a, &b, &c);
 
-    if(a < b && b < c)printf("%d %d %d\n", a, b, c);
-    if(a < c && c < b)printf("%d %d %d\n", a, c, b);
-    if(b < a && a < c)printf("%d %d %d\n", b, a, c);
-    if(b < c && c < a)printf("%d %d %d\n", b, c, a);
-    if(c < a && a < b)printf("%d %d %d\n", c, a, b);
-    if(c < b && b < a)printf("%d %d %d\n", c, b, a);
+    if(a <= b && b <= c) printf("%d %d %d\n", a, b, c);
+    else if(a <= c && c <= b) printf("%d %d %d\n", a, c, b);
+    else if(b <= a && a <= c) printf("%d %d %d\n", b, a, c);
+    else if(b <= c && c <= a) printf("%d %d %d\n", b, c, a);
+    else if(c <= a && a <= b) printf("%d %d %d\n", c, a, b);
+    else if(c <= b && b <= a) printf("%d %d %d\n", c, b, a);
 
     return 0;
 }
